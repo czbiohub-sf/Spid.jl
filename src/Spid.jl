6baa6dcde100854e9c4310bf2f76cda160036db7
Jack Kamm <jackkamm@gmail.com>
2019-08-03 05:29:21 +0000

    Refactor filter variant sites, to allow more general filter function

--- src/Spid.jl
+++ src/Spid.jl
@@ -13,6 +13,52 @@ const skip_chars = "N-";
 using ArgParse
 using CSV
 
+function read_fasta!(fasta, names, seqs)
+    for record in FASTA.Reader(fasta)
+        name = FASTA.identifier(record)
+        seq = String(sequence(CharSequence, record))
+        push!(names, name)
+        push!(seqs, seq)
+    end
+end
+
+function filter_fasta_sites(in_fasta, filter_fun)
+    names = String[]
+    seqs = String[]
+    read_fasta!(in_fasta, names, seqs);
+
+    for seq in seqs
+        @assert length(seq) == length(seqs[1])
+    end
+
+    filtered_sites = filter_seqs_sites(seqs, filter_fun)
+    new_seqs = [Char[] for s in seqs]
+    for site in filtered_sites
+        for (i, allele) in enumerate(site)
+            push!(new_seqs[i], allele)
+        end
+    end
+
+    return [
+        FASTA.Record(name, CharSequence(join(seq)))
+        for (name, seq) in zip(names, new_seqs)
+    ]
+end
+
+function filter_seqs_sites(seqs, filter_fun)
+    filtered_sites = Vector{Vector{Char}}()
+    observed_alleles = Set(Char[])
+    for pos in 1:length(seqs[1])
+        column = [seq[pos] for seq in seqs]
+        if filter_fun(column)
+            push!(filtered_sites, column)
+        end
+    end
+    return filtered_sites
+end
+
+export filter_fasta_sites
+
 include("FastaEditDistances.jl")
 export fasta_edit_distances
 
--- src/VariantsOnlyFasta.jl
+++ src/VariantsOnlyFasta.jl
@@ -1,47 +1,11 @@
 function get_variant_only_seqs(in_fasta)
-    names = String[]
-    seqs = String[]
-    read_fasta!(in_fasta, names, seqs);
-
-    for seq in seqs
-        @assert length(seq) == length(seqs[1])
-    end
-
-    variants = get_variants(seqs)
-    new_seqs = [Char[] for s in seqs]
-    for variant in variants
-        for (i, allele) in enumerate(variant)
-            push!(new_seqs[i], allele)
-        end
-    end
-
-    return [
-        FASTA.Record(name, CharSequence(join(seq)))
-        for (name, seq) in zip(names, new_seqs)
-    ]
+    return filter_fasta_sites(in_fasta, is_variant_site)
 end
 
-function get_variants(seqs)
-    variants = Vector{Vector{Char}}()
+function is_variant_site(column)
     observed_alleles = Set(Char[])
-    for pos in 1:length(seqs[1])
-        column = [seq[pos] for seq in seqs]
-        empty!(observed_alleles)
-        fill_observed_alleles!(observed_alleles, column);
-        if length(observed_alleles) >= 2
-            push!(variants, column)
-        end
-    end
-    return variants
-end
-
-function read_fasta!(fasta, names, seqs)
-    for record in FASTA.Reader(fasta)
-        name = FASTA.identifier(record)
-        seq = String(sequence(CharSequence, record))
-        push!(names, name)
-        push!(seqs, seq)
-    end
+    fill_observed_alleles!(observed_alleles, column);
+    return (length(observed_alleles) >= 2)
 end
 
 function fill_observed_alleles!(observed_alleles, column)
