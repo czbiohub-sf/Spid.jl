4e7939fd3c08c043f307067c45ced8aff78236e5
Jack Kamm <jackkamm@gmail.com>
2019-08-27 17:22:46 +0000

    Remove old scripts

--- bin/multi_alignment_distances.jl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env julia
-using CSV
-using Spid
-
-in_fasta, out_csv = ARGS
-CSV.write(out_csv, fasta_edit_distances(in_fasta))
--- bin/multi_alignment_variants.jl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env julia
-using BioSequences
-using Spid
-
-w = FASTA.Writer(stdout)
-for record in get_variant_only_seqs(stdin)
-    write(w, record)
-end
